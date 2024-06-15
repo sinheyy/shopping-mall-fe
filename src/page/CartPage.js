02a2a7e53ed6016ac889ee31b80cbdcd0c94b97d
sinhey <sinheyyy@gmail.com>
2024-06-16 05:13:53 +0900

    fix loading cart

--- src/page/CartPage.js
+++ src/page/CartPage.js
@@ -15,39 +15,34 @@ const CartPage = () => {
   useEffect(() => {
     //카트리스트 불러오기
     dispatch(cartActions.getCartList());
-  }, []);
+  }, [dispatch]);
 
-  if (loading) {
-    return (<div className='loading'><ClipLoader color="#FB6D33" loading={loading} size={100} /></div>);
-  }
-  else {
-    return (
-      <Container>
-        <Row>
-          <Col xs={12} md={7}>
-            {
-              cartList.length > 0 ?
-                (
-                  cartList.map((item) => (
-                    <CartProductCard item={item} key={item._id} />
-                  ))
-                )
-                :
-                (
-                  <div className="text-align-center empty-bag">
-                    <h2>카트가 비어있습니다.</h2>
-                    <div>상품을 담아주세요!</div>
-                  </div>
-                )
-            }
-          </Col>
-          <Col xs={12} md={5}>
-            <OrderReceipt cartList={cartList} totalPrice={totalPrice} totalSalePrice={totalSalePrice} totalProductPrice={totalProductPrice} />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} md={7}>
+          {
+            cartList.length > 0 ?
+              (
+                cartList.map((item) => (
+                  <CartProductCard item={item} key={item._id} />
+                ))
+              )
+              :
+              (
+                <div className="text-align-center empty-bag">
+                  <h2>카트가 비어있습니다.</h2>
+                  <div>상품을 담아주세요!</div>
+                </div>
+              )
+          }
+        </Col>
+        <Col xs={12} md={5}>
+          <OrderReceipt cartList={cartList} totalPrice={totalPrice} totalSalePrice={totalSalePrice} totalProductPrice={totalProductPrice} />
+        </Col>
+      </Row>
+    </Container>
+  );
 
 
 };
